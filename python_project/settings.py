349a09a19fb3b91b93ebb610738d607ec0099323
John Weisz <johnmweisz@gmail.com>
2019-10-02 18:08:37 -0700

    added docker support but fails to deploy, working on fix

--- python_project/settings.py
+++ python_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!634#xj&g=%6tgk7o&h+zt*r&m^1ov%u@h*@hylfl*wh*2@p2='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- python_project/urls.py
+++ python_project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 from apps.user_app.models import User as U
 class UAdmin(admin.ModelAdmin):
     pass
